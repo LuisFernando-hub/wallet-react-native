b344fffda8ab1b4554d4d769945faf7c293d599d
Luis Fernando <84160974+LuisFernando-hub@users.noreply.github.com>
2026-03-06 18:47:12 +0400

    ajust build

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -4,9 +4,16 @@ import { Slot } from 'expo-router'
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import { StatusBar } from "expo-status-bar";
 
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
 export default function RootLayout() {
+  
+  if (!publishableKey) {
+    throw new Error("Erro: A variável EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY não foi encontrada.");
+  }
+
   return (
-    <ClerkProvider tokenCache={tokenCache}>
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
       <SafeScreen>
         <Slot />
     </SafeScreen>
