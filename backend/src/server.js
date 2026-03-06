8dec912ea1f3671b3070361c5134db7953f64aa1
Luis Fernando <84160974+LuisFernando-hub@users.noreply.github.com>
2026-03-06 19:12:42 +0400

    ajust build

--- backend/src/server.js
+++ backend/src/server.js
@@ -40,7 +40,6 @@ app.use(async (req, res, next) => {
     next();
 });
 
-// Para rodar LOCALMENTE (não afeta a Vercel)
 if (process.env.NODE_ENV !== "production") {
     const PORT = process.env.PORT || 5001;
     initDB().then(() => {
