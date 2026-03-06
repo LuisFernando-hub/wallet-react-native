57024511b05f01228049d6949cad359081879f59
Luis Fernando <84160974+LuisFernando-hub@users.noreply.github.com>
2026-03-06 21:12:56 +0400

    ajust build 2

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://wallet-api-ufnl.onrender.com/api";
+export const API_URL = "https://wallet-react-native-1.onrender.com/api";
 // export const API_URL = "http://localhost:5001/api";
