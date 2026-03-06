b3e6a08d80e24d2f24e2d92c836c7703810c89c2
Luis Fernando <84160974+LuisFernando-hub@users.noreply.github.com>
2026-03-06 21:07:57 +0400

    ajust build 2

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,15 +13,15 @@ const app = express();
 
 // Middleware
 // app.use(rateLimiter);
-// app.use(cors());
-// app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
-// app.use((req,res,next) => {
-//     console.log("Hey we hit a req, the method is", req.method)
-//     next();
-// })
+app.use((req,res,next) => {
+    console.log("Hey we hit a req, the method is", req.method)
+    next();
+})
 
-// const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001;
 
 app.get("/", (req, res) => {
     res.status(200).json({
@@ -29,10 +29,10 @@ app.get("/", (req, res) => {
     });
 })
 
-// app.use("/api/transactions", transactionsRoute);
+app.use("/api/transactions", transactionsRoute);
 
-// initDB().then(() => {
-//     app.listen(PORT, () => {
-//         console.log("Server is running on port", PORT);
-//     });
-// })
+initDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server is running on port", PORT);
+    });
+})
